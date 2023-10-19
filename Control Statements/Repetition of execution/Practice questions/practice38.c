d38b5ab7dcba4e435288183d181b8da76c7b4cdf
Nazmus Sakib <147674770+nazmusweb-coding@users.noreply.github.com>
2023-10-19 22:34:13 +0600

    Empty-Commit

--- Control Statements/Repetition of execution/Practice questions/practice38.c
+++ Control Statements/Repetition of execution/Practice questions/practice38.c
@@ -2,13 +2,15 @@
 #include <stdio.h>
 void main (void) {
     int n;
+    int factor = 0;
     printf("Input any number: ");
     scanf("%d", &n);
-    for (int i = 2; i < n; i++)
-    {
-        if (n%i!=0 || n == 1 || n == 2 || n== 3)
-            printf("%d is prime number", n);
-            break;
-    }
-    
+    for (int i = 1; i <=n; i++)
+        if (n%i==0)
+            factor++;    
+    if (factor==2)                  // if the number has only 2 factors then its prime
+        printf("%d is prime number.\n", n);
+    else                            // else if has more then 2 factors or less then 2 factors its not prime
+        printf("%d is not prime number.\n", n);
+
 }
