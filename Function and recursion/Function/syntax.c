cf892d0bee536f88471d4cdb2d9af03b72e9b38f
Nazmus Sakib <147674770+nazmusweb-coding@users.noreply.github.com>
2023-10-27 00:22:19 +0600

    folder update

---
+++ Function and recursion/Function/syntax.c
@@ -0,0 +1,16 @@
+// syntax of function
+#include <stdio.h>
+
+// step one: function declaration/prototype
+void function(/*a functioncan contain more then one parameters or no parameters*/);
+
+void main() {
+    
+    // step three: function call by passing arguments or no arguments
+    function();
+}
+
+// step three: function definition
+void function(){
+
+}
