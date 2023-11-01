ec0a6f8a924ae172507d4ff5340687f5eeebebd6
Nazmus Sakib <147674770+nazmusweb-coding@users.noreply.github.com>
2023-11-01 20:20:10 +0600

    Folder rearrange

---
+++ Control Statements/Repetition of execution/Number pattern exercises codeforwin/Triangle Easy Number Patterns/num_pattern_20.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+void pattern(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d ", i);
+        }
+        printf("\n");
+    }
+    
+}
+void main()
+{
+    int n;
+    scanf("%d", &n);
+    pattern(n);
+}
