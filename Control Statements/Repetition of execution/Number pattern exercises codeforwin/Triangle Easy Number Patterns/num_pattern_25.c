75b6f21db5236c46de381ef57f5c45b9e8215695
Nazmus Sakib <147674770+nazmusweb-coding@users.noreply.github.com>
2023-11-02 01:00:29 +0600

    Problem solved

---
+++ Control Statements/Repetition of execution/Number pattern exercises codeforwin/Triangle Easy Number Patterns/num_pattern_25.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void pattern(int n)
+{
+    for (int i = n; i >= 1 ; i--)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%d ", j);
+        }
+        printf("\n");
+    }
+}
+void main()
+{
+    int n;
+    scanf("%d", &n);
+    pattern(n);
+}
