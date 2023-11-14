2496e7ba2a1331b8c3b38496fffe08d2c173912c
Nazmus Sakib <147674770+nazmusweb-coding@users.noreply.github.com>
2023-11-14 21:20:20 +0600

    Problem solved

---
+++ Control Statements/Repetition of execution/Number pattern exercises codeforwin/Triangle Easy Number Patterns/num_pattern_45.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+void pattern(int n)
+{
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            /* code */
+        }
+        
+    }
+    
+}
