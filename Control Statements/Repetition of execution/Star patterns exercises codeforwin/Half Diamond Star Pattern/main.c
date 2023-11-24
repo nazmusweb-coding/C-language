76221b8883e8825d4a1e44b55a162df535ce4fe2
Nazmus Sakib <147674770+nazmusweb-coding@users.noreply.github.com>
2023-11-25 01:32:25 +0600

    problem solved

---
+++ Control Statements/Repetition of execution/Star patterns exercises codeforwin/Half Diamond Star Pattern/main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+void pattern (int n) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (int i = 1; i < n; i++)
+    {
+        for (int j = n-1; j >= i; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+}
+int main () {
+    int n;
+    scanf("%d", &n);
+    pattern(n);
+    return 0;
+}
