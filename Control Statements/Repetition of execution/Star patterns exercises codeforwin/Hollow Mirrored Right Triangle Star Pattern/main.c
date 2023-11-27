b9101ada90d31eb81cb225d3c5002e2bd869ddde
Nazmus Sakib <147674770+nazmusweb-coding@users.noreply.github.com>
2023-11-28 00:14:46 +0600

    Problem Solved

---
+++ Control Statements/Repetition of execution/Star patterns exercises codeforwin/Hollow Mirrored Right Triangle Star Pattern/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+void pattern (int n) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j >= i; j--)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= i; k++)
+        {
+            if (k==1 || k==i || i==n)
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        printf("\n");
+    }
+    
+}
+int main () {
+    int n;
+    scanf("%d", &n);
+    pattern(n);
+    return 0;
+}
