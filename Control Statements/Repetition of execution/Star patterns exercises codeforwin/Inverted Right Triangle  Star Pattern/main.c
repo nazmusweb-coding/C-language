1d704600b4a8d7da19047c4b9404017ddfd7ae82
Nazmus Sakib <147674770+nazmusweb-coding@users.noreply.github.com>
2023-12-01 00:13:22 +0600

    problem solved

---
+++ Control Statements/Repetition of execution/Star patterns exercises codeforwin/Inverted Right Triangle  Star Pattern/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void pattern (int n) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = n; j >= i; j--)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+}
+int main () {
+    int n;
+    scanf("%d", &n);
+    pattern(n);
+    return 0;
+}
