15eadf18749134837387abb646c5bad58d791c0d
Nazmus Sakib <147674770+nazmusweb-coding@users.noreply.github.com>
2023-12-03 10:17:48 +0600

    Problem solved

---
+++ Control Statements/Repetition of execution/Star patterns exercises codeforwin/Mirrored Rhombus Star Pattern/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+void pattern (int n) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            printf(" ");
+        }
+        for (int k = 1; k <= n; k++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+    
+}
+int main () {
+    int n;
+    scanf("%d", &n);
+    pattern(n);
+    return 0;
+}
