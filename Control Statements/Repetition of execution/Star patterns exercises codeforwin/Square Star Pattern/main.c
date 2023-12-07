ca52eb2fe3ffa0e78396ea86553c8973e9a558bb
Nazmus Sakib <147674770+nazmusweb-coding@users.noreply.github.com>
2023-12-07 08:40:05 +0600

    Problem Solved

---
+++ Control Statements/Repetition of execution/Star patterns exercises codeforwin/Square Star Pattern/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+void pattern (int n) {
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= n; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+}
+int main () {
+    int n;
+    scanf("%d", &n);
+    pattern(n);
+    return 0;
+}
